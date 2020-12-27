33fd9e49be8ff9869a367a8e940f32933bd4f6af
Dylan Black <dylan.black2021@gmail.com>
2020-12-27 15:42:05 -0500

    Movie picking functionality

--- components/MoviePicker.jsx
+++ components/MoviePicker.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TextField, Button } from '@material-ui/core'
 
 export default function MoviePicker() {
   const [movies, setMovies] = useState([]);
   const [movie, setMovie] = useState("");
+  const [chosenMovie, setChosenMovie] = useState("");
+
+  useEffect(() => {
+    //When the user adds/removes a movie, reset the input and allow them to choose a new movie.
+    setChosenMovie("");
+    setMovie("");
+  }, [movies])
 
   const addMovie = () => {
-    setMovies(movie && [...movies, movie])
-    setMovie("");
+    setMovies(movie && [...new Set([...movies, movie])])
   }
 
   const removeMovie = (event) => {
@@ -27,24 +33,41 @@ export default function MoviePicker() {
     }
   }
 
+  const chooseMovie = () => {
+    // "~~" for a closest "int"
+    if(!chosenMovie) {
+      setChosenMovie(movies[~~(movies.length * Math.random())]);
+    }
+  }
+
+  const reset = () => {
+    setMovies([]);
+    setMovie("");
+    setChosenMovie("");
+  }
+
   return (
     <div>
       <form autoComplete="off">
         <TextField id="outlined-basic" label="Enter A Movie..." variant="outlined" onChange={handleInput} value={movie} onKeyDown={preventRefresh} />
-        <Button variant="contained" onClick={addMovie}>Add Movie</Button>
+        <Button variant="contained" disabled={!movie} onClick={addMovie}>Add Movie</Button>
       </form>
-      {movies.length > 0 &&
+      {movies.length > 0 && 
         <div>
           <p>Click to remove a movie</p>
           <ol>
-            {[...new Set(movies)].map(movie => (
+            {movies.map(movie => (
               <li key={movie} onClick={removeMovie}>{movie}</li>
             ))}
           </ol>
-          <Button variant="contained">Choose a Movie!</Button>
-        </div>
+          <Button variant="contained" onClick={chooseMovie}>Choose a Movie!</Button>
+          
+          {chosenMovie &&
+          <h1>You will be watching...{chosenMovie}!</h1>}
+        
+          <Button variant="contained" onClick={reset}>Reset</Button>
+        </div>   
       }
-      
     </div>
   )
 }
