78dea2187e58a13dfd6689828e2cc5843b61d7c8
Dylan Black <dylan.black2021@gmail.com>
2020-12-28 01:21:25 -0500

    Quick styling fixes + add title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import MoviePicker from '../components/MoviePicker'
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Movie Picker</title>
+      </Head>
+
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Can't Decide What To Watch?</h1>
         <h2 className={styles.subtitle}>Enter Movies Below And Let Technology Decide Your Fate...</h2>
