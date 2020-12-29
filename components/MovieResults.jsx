21047b75cc79e2e858af6c151d3d0c407f1f7a7a
Dylan Black <dylan.black2021@gmail.com>
2020-12-28 19:38:54 -0500

    Results issues

--- components/MovieResults.jsx
+++ components/MovieResults.jsx
@@ -7,32 +7,40 @@ export default function MovieResults({ movieData }) {
 
   return (
     <div className={styles.resultsContainer}>
-      <img src={`https://image.tmdb.org/t/p/w500${poster}`} />
+      {poster != null && <img src={`https://image.tmdb.org/t/p/w500${poster}`} />}
       <h1>You'll be watching...</h1>
       <h1>{title}!</h1>
-      <p>{description}</p>
+      {description.length > 0 && <p>{description}</p>}
       <div className={styles.genreAndStars}>
-        <div className={styles.genres}>
-          {genres.length === 1 ?
-            <h2 className={styles.subtitle}>Genre: {genres[0]}</h2>
-            :
-            <div className={styles.listContainer}>
-              <h2 className={styles.subtitle}>Genres:</h2>
-              <ul>
-                {genres.map(genre => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          }
-        </div>
-        
+        {genres.length > 0 && 
+          <div className={styles.genres}>
+            {genres.length === 1 ?
+              <h2 className={styles.subtitle}>Genre: {genres[0]}</h2>
+              :
+              <div className={styles.listContainer}>
+                <h2 className={styles.subtitle}>Genres:</h2>
+                <ul>
+                  {genres.map(genre => (
+                    <li key={genre}>{genre}</li>
+                  ))}
+                </ul>
+              </div>
+            } 
+          </div>
+        }
         <div className={styles.stars}>
           <h2 className={styles.subtitle}>What did critics think?</h2>
-          <div style={{ fontSize: '50px' }}>
-            <StarRatingComponent name="rating" value={stars} starCount={10} editing={false} />
-          </div>
-          <h3 style={{ marginTop: '-10px' }}>{stars}/10 stars</h3>
+          {stars > 0 ?
+            <div>
+              <div style={{ fontSize: '50px' }}>
+                <StarRatingComponent name="rating" value={stars} starCount={10} editing={false} />
+              </div>
+              <h3 style={{ marginTop: '-10px' }}>{stars}/10 stars</h3>
+            </div>           
+          :
+            <h2>Not enough critics rated this movie 😢</h2>
+          }
+
         </div>
       </div>
       
